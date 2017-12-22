b1259abdd0a60623c23c4d6df9fc0545d52bd6d0
Jason Galea <jason@lecstor.com>
2017-12-22 17:06:11 +1000

    add specific feature examples

--- example.js
+++ example.js
@@ -1,30 +1,48 @@
 const chalk = require("chalk");
 const Prompt = require("./index");
 
-const debug = require("./debug");
-
-const autoComplete = require("./key-press-autocomplete");
+const AutoComplete = require("./key-press-autocomplete");
 const history = require("./key-press-history");
 
+const complete = AutoComplete(["abc1", "ab12", "abcdefg", "a123"]);
+
 const prompt = Prompt({ prompt: chalk`{blue default> }` });
-
-debug({ promptState: prompt.state });
-
-Object.assign(prompt, {
-  keyPressers: [...prompt.keyPressers, autoComplete, history]
-});
-
-const options = {};
-//   header: "What do you think?",
-//   prompt: "answer> ",
-//   footer: "There is no spoon"
-// };
+const defaultKP = [...prompt.keyPressers];
 
 function newPrompt() {
-  return prompt.start(options).then(command => {
-    console.log(`received: "${command}"`);
-    return newPrompt();
-  });
+  Object.assign(prompt, { keyPressers: [...defaultKP, history] });
+  return prompt
+    .start({
+      header: "Can have a header",
+      prompt: "thePrompt> ",
+      footer: "Can have a footer"
+    })
+    .then(command => console.log(`received: "${command}"`))
+    .then(() =>
+      prompt.start({
+        header: "Can have a default",
+        prompt: "thePrompt> ",
+        default: "help"
+      })
+    )
+    .then(command => console.log(`received: "${command}"`))
+    .then(() =>
+      prompt.start({
+        header: "Can be a secret",
+        prompt: "thePrompt> ",
+        secret: true
+      })
+    )
+    .then(command => console.log(`received: "${command}"`))
+    .then(() => {
+      Object.assign(prompt, { keyPressers: [...defaultKP, complete, history] });
+      return prompt.start({
+        header: chalk`Can update footer/prompt/header and do autocomplete {grey (try abcd [tab])}`,
+        prompt: "thePrompt> "
+      });
+    })
+    .then(command => console.log(`received: "${command}"`))
+    .then(newPrompt);
 }
 
 newPrompt().catch(console.error);

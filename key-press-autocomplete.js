a794df8f46b957a66c23c31992d75cb9383e56d7
Jason Galea <jason@lecstor.com>
2017-12-22 17:02:16 +1000

    more than just a demo

--- key-press-autocomplete.js
+++ key-press-autocomplete.js
@@ -1,30 +1,31 @@
+const _filter = require("lodash/filter");
 const { applyPatch } = require("./immutably.js");
 
-const debug = require("./debug");
+function keyPressAutoComplete(choices) {
+  return {
+    keyPress: async function(state, press) {
+      if (state.mode.command) {
+        let command = state.command.text;
 
-const keyPressAutoComplete = {
-  keyPress(state, press) {
-    if (state.mode !== "command") {
+        const matches = _filter(choices, choice => choice.startsWith(command));
+
+        if (press.key.name === "tab" && matches.length === 1) {
+          // complete the command
+          const parts = command.split(" ");
+          parts.pop();
+          command = parts.length ? `${parts.join(" ")} ` : ``;
+          const text = `${command}${matches[0]} `;
+          state = applyPatch(state, {
+            command: { text },
+            cursor: { col: null }
+          });
+        } else {
+          state = applyPatch(state, { footer: matches.join(" ") });
+        }
+      }
       return state;
     }
-    const command = state.command.text;
-    debug({ command: `"${command}"` });
-    if (press.key.name === "tab") {
-      // complete the command
-      const text = `${command}completed`;
-      return applyPatch(state, {
-        command: {
-          text
-        },
-        cursor: { col: null }, // tell render to calculate cursor position
-        footer: ""
-      });
-    }
-
-    return applyPatch(state, {
-      footer: "possible command completions possible command completions"
-    });
-  }
-};
+  };
+}
 
 module.exports = keyPressAutoComplete;
